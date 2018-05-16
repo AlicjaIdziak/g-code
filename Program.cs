fa8a9b390d01a3735d3d17bccba22c8c1f7bf95a
AlicjaIdziak <39094030+AlicjaIdziak@users.noreply.github.com>
2018-05-16 20:35:40 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -22,16 +22,9 @@ namespace _3dprinter_gcode
                 Console.WriteLine("Your G Value:");
                 int g = int.Parse(Console.ReadLine());
                 decimal e = 850 / 100;
-                //decimal eDiag = 1202.0815m / 100;
                 decimal k = 0;
-                if (g == 0 || g == 1)
+                if (g == 1)
                 {
-
-                    //formula to determine after how many horizontal steps we get a vertical step or vice versa
-
-                    //if statement to determine the amount of extrution needed based on step direction
-                    //horizonatl and vertical steps receive different amount of extrution than diagonal
-
 
                     for (decimal i = 0; i < z; i += 0.005m)
                     {
